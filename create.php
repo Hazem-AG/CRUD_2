8c4f44d7dbdbb1f3497a33b253bfc2f644d99dd1
ahmedanwar1234 <ahmedanswar123@gmail.com>
2024-08-03 12:29:31 +0300

    aaaaaaaaa

--- create.php
+++ create.php
@@ -1,11 +1,12 @@
 <?php
-include 'db.php';
+include 'Conn.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $title = $_POST['title'];
-    $description = $_POST['description'];
+    $Name = $_POST['Name'];
+    $Email = $_POST['Email'];
+    $Phone = $_POST['Phone'];
 
-    $sql = "INSERT INTO tasks (title, description) VALUES ('$title', '$description')";
+    $sql = "INSERT INTO `t-crud` (`id`,`Name`, `Phone`) VALUES (NULL,'$Name', '$Phone','$Email')";
     if ($conn->query($sql) === TRUE) {
         echo "New record created successfully";
     } else {
